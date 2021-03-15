0c2c79e17e9f9d78f247ed89ff306d61e9fbcfa3
catgsmith <catgsmith@hotmail.com>
2021-03-15 20:05:21 +0000

    add back navigation

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { } // private router: Router (replaced)
 
   ngOnInit() {
   }
+
+  onBookPlace() {
+    // choose 1 of three options for back navigation:-
+    //this.router.navigateByUrl('/places/tabs/discover'); // 1. but shows forward animation 
+    this.navCtrl.navigateBack('/places/tabs/discover');   // 2. improved by showing backward animation
+
+    //this.navCtrl.pop(); // 3. but popping doesn't work if the stack of pages is empty
+  }
+
 
 }
