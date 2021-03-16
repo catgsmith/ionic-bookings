f96c1f483332f8962e65d011a60f39e4b0ae6cdf
catgsmith <catgsmith@hotmail.com>
2021-03-16 21:56:40 +0000

    passing data from modal

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -8,10 +11,18 @@ import { CreateBookingComponent } from '../../../bookings/create-booking/create-
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  place: Place;
 
-  constructor(private navCtrl: NavController, private modalCtrl: ModalController) { } // private router: Router (replaced)
+  constructor(private route: ActivatedRoute, private placesService: PlacesService, private navCtrl: NavController, private modalCtrl: ModalController) { } // private router: Router (replaced)
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
   async onBookPlace() {
@@ -20,8 +31,17 @@ export class PlaceDetailPage implements OnInit {
     // this.navCtrl.navigateBack('/places/tabs/discover'); // 2. improved by showing backward animation
     //this.navCtrl.pop(); // 3. but popping doesn't work if the stack of pages is empty
     const modal = await this.modalCtrl.create({
-      component: CreateBookingComponent
-    });   
+      component: CreateBookingComponent,
+      componentProps: { selectedPlace: this.place }
+    }); 
+    
+    modal.onDidDismiss()
+      .then((resultData) => {
+          console.log(resultData.data, resultData.role);
+          if (resultData.role === 'confirm') {
+            console.log('BOOKED!');
+          }
+      });
     return await modal.present();
   }
 }
