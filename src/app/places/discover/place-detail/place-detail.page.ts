199075cda10c9f88caa4d3d53397859c1d216288
catgsmith <catgsmith@hotmail.com>
2021-03-16 21:11:02 +0000

    add booking modal

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { ModalController, NavController } from '@ionic/angular';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -8,18 +9,19 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private navCtrl: NavController) { } // private router: Router (replaced)
+  constructor(private navCtrl: NavController, private modalCtrl: ModalController) { } // private router: Router (replaced)
 
   ngOnInit() {
   }
 
-  onBookPlace() {
+  async onBookPlace() {
     // choose 1 of three options for back navigation:-
-    //this.router.navigateByUrl('/places/tabs/discover'); // 1. but shows forward animation 
-    this.navCtrl.navigateBack('/places/tabs/discover');   // 2. improved by showing backward animation
-
+    // this.router.navigateByUrl('/places/tabs/discover'); // 1. but shows forward animation 
+    // this.navCtrl.navigateBack('/places/tabs/discover'); // 2. improved by showing backward animation
     //this.navCtrl.pop(); // 3. but popping doesn't work if the stack of pages is empty
+    const modal = await this.modalCtrl.create({
+      component: CreateBookingComponent
+    });   
+    return await modal.present();
   }
-
-
 }
