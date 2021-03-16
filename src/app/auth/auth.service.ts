c51456ee2c84d5bc86a19c78272b5b044890126e
catgsmith <catgsmith@hotmail.com>
2021-03-16 20:16:14 +0000

    login logout navigation with guard

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _userIsAuthenticated = false;
+
+  public get userIsAuthenticated() {
+    return this._userIsAuthenticated;
+  }
+  public set userIsAuthenticated(value) {
+    this._userIsAuthenticated = value;
+  }
+
+  constructor() { }
+
+  login() {
+      this._userIsAuthenticated = true;
+  }
+  
+  logout() {
+      this._userIsAuthenticated =  false;
+  }
+}
