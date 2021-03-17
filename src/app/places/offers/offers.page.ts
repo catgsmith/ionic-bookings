33c9c6773e712fc992d21d18d3c283f99706a833
catgsmith <catgsmith@hotmail.com>
2021-03-17 17:20:54 +0000

    add swipable item to Edit option

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
@@ -9,10 +11,15 @@ import { PlacesService } from '../places.service';
 })
 export class OffersPage implements OnInit {
   offers: Place[];
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     this.offers = this.placesService.getPlaces();
   }
 
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    //console.log('Editing item: ', offerId);
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId])
+  }
 }
