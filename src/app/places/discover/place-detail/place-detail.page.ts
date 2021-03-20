cbfb8c4f27a468dc723acd1bd9368d8d3b529db0
catgsmith <catgsmith@hotmail.com>
2021-03-20 23:25:51 +0000

    using action sheet controller

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ModalController, NavController } from '@ionic/angular';
+import { ActionSheetController, ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
@@ -13,10 +13,16 @@ import { PlacesService } from '../../places.service';
 export class PlaceDetailPage implements OnInit {
   place: Place;
 
-  constructor(private route: ActivatedRoute, private placesService: PlacesService, private navCtrl: NavController, private modalCtrl: ModalController) { } // private router: Router (replaced)
+  constructor(
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
+    private navCtrl: NavController,
+    private modalCtrl: ModalController,
+    private actionSheetCtrl: ActionSheetController
+  ) {} // private router: Router (replaced)
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
+    this.route.paramMap.subscribe((paramMap) => {
       if (!paramMap.has('placeId')) {
         this.navCtrl.navigateBack('/places/tabs/offers');
         return;
@@ -25,23 +31,52 @@ export class PlaceDetailPage implements OnInit {
     });
   }
 
-  async onBookPlace() {
+  onBookPlace() {
     // choose 1 of three options for back navigation:-
-    // this.router.navigateByUrl('/places/tabs/discover'); // 1. but shows forward animation 
+    // this.router.navigateByUrl('/places/tabs/discover'); // 1. but shows forward animation
     // this.navCtrl.navigateBack('/places/tabs/discover'); // 2. improved by showing backward animation
     //this.navCtrl.pop(); // 3. but popping doesn't work if the stack of pages is empty
+
+    this.actionSheetCtrl.create({
+      header: '',
+      buttons: [
+        {
+          text: 'Select Date',
+          handler: () => {
+            this.openBookingModal('select');
+          }
+        },
+        {
+          text: 'Random Date',
+          handler: () => {
+            this.openBookingModal('random');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    })
+    .then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+  }
+
+  async openBookingModal(mode: 'select' | 'random') {
+    console.log(mode);
+    
     const modal = await this.modalCtrl.create({
       component: CreateBookingComponent,
-      componentProps: { selectedPlace: this.place }
-    }); 
-    
-    modal.onDidDismiss()
-      .then((resultData) => {
-          console.log(resultData.data, resultData.role);
-          if (resultData.role === 'confirm') {
-            console.log('BOOKED!');
-          }
-      });
-    return await modal.present();
+      componentProps: { selectedPlace: this.place },
+    });
+
+    modal.onDidDismiss().then((resultData) => {
+      console.log(resultData.data, resultData.role);
+      if (resultData.role === 'confirm') {
+        console.log('BOOKED!');
+      }
+    });
+    return await modal.present();  
   }
 }
