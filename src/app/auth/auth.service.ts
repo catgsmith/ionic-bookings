35cfb45a94d09b419fa810531d0ffe32e5a6993f
catgsmith <catgsmith@hotmail.com>
2021-03-22 19:50:36 +0000

    Add offer item and theme with CSS

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private _userIsAuthenticated = false;
+  private _userIsAuthenticated = true;
 
   public get userIsAuthenticated() {
     return this._userIsAuthenticated;
